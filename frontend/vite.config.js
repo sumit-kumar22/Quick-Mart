d425d913e01285defe075e7cc759cbb953e86e37
SUMIT KUMAR <sumityadav4623@gmail.com>
2026-09-24 12:57:23 +0530

    fix: prepare frontend for vercel deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ command }) => ({
-  base: command === 'serve' ? '/' : '/Quick-Mart/',
   plugins: [react()],
   build: {
     rollupOptions: {
